ae48415f05f0b9ed73f4410ec6f37c4d41be341e
Julien Mathis <jmathis@centreon.com>
2016-05-23 19:47:11 +0200

    fix problem with the height of the widget

--- servicegroup-monitoring/data.js
+++ servicegroup-monitoring/data.js
@@ -43,10 +43,10 @@ function loadPage()
 {
     jQuery.ajax("./src/index.php?widgetId=" + widgetId + "&page=" + pageNumber, {
         success: function (htmlData) {
-            //jQuery("#sgMonitoringTable").html("");
+            jQuery("#sgMonitoringTable").html("");
             jQuery("#sgMonitoringTable").html(htmlData);
             var monitoringTable = jQuery("#sgMonitoringTable").find("img, style, script, link").load(function() {
-                var h = document.getElementById("sgMonitoringTable").scrollHeight*2;
+                var h = document.getElementById("sgMonitoringTable").scrollHeight+2;
                 ResizeFrame(window.name, h);
             });
         }
@@ -59,28 +59,11 @@ function loadPage()
     }
 }
 
-jQuery(function() {
-    loadPage();
-    $('.checkall').live('click', function () {
-            var chck = this.checked;
-            $(this).parents().find(':checkbox').each(function() {
-                    $(this).attr('checked', chck);
-                    clickedCb[$(this).attr('id')] = chck;
-                });
-        });
-    $(".selection").live('click', function() {
-            clickedCb[$(this).attr('id')] = this.checked;
-        });
-});
-
-
 function ResizeFrame(ifrm, height)
 {
-    /*
+    
     if (height < 150) {
         height = 150;
     }
-    */
-    console.log("SIZE:"+height);
-    jQuery("[name=" + ifrm + "]").height(height);
+    parent.iResize(window.name, height);
 }
